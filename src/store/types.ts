d6449c32c94e100c12968a0e74135639d237bf7a
KAMRONBEK <kamuranbek1998@gmail.com>
2022-06-23 20:34:41 +0500

    added change quantity type

--- src/store/types.ts
+++ src/store/types.ts
@@ -2,3 +2,4 @@ export const LOAD_PRODUCTS = 'LOAD_PRODUCTS';
 export const ADD_ITEMS = 'ADD_ITEMS';
 export const REMOVE_ITEM = 'REMOVE_ITEM';
 export const CLEAR_ALL_ITEMS = 'CLEAR_ALL_ITEMS';
+export const CHANGE_QUANTITY="CHANGE_QUANTITY"
