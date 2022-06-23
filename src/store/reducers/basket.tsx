f12013839c057ffe3fba4fe64b87705f271f31c9
KAMRONBEK <kamuranbek1998@gmail.com>
2022-06-23 20:31:22 +0500

    added functionality for adding quantity and changing quantity

--- src/store/reducers/basket.tsx
+++ src/store/reducers/basket.tsx
@@ -1,27 +1,54 @@
-import {REMOVE_ITEM, ADD_ITEMS, CLEAR_ALL_ITEMS} from '../types';
+import {addItems} from '../actions/basket';
+import {
+  REMOVE_ITEM,
+  ADD_ITEMS,
+  CLEAR_ALL_ITEMS,
+  CHANGE_QUANTITY,
+} from '../types';
 
 const basketState = {
-    basket_products: [],
+  basketProducts: [],
 };
 
 export const basketReducer = (state = basketState, action?: any) => {
   switch (action.type) {
     case REMOVE_ITEM:
-      const removeItems = state.basket_products.filter(
+      const removeItems = state.basketProducts.filter(
         item => item.id !== action.payload,
       );
       return {
-        ...state,
-        basket_products: removeItems,
+        basketProducts: removeItems,
       };
       break;
     case ADD_ITEMS:
-      const basket_products = [...state.basket_products, action.payload];
+      let id = state.basketProducts.some(item =>
+        item.id == action.payload.id ? item.id : false,
+      );
+      let item = action.payload;
+      item.quantity = 1;
+      const basketProducts = id
+        ? state.basketProducts.map(item => {
+            item.quantity += 1;
+            return item;
+          })
+        : [...state.basketProducts, item];
       return {
-        ...state,
-        basket_products: basket_products,
+        basketProducts: basketProducts,
       };
       break;
+
+    case CHANGE_QUANTITY:
+      let newProducts = state.basketProducts.map(item => {
+        if (item.id == action.payload.id) {
+          console.log(item.quantity + action.payload.number);
+
+          item.quantity = Math.max(item.quantity + action.payload.number, 0);
+        }
+        return item;
+      });
+      return {
+        basketProducts: newProducts,
+      };
     case CLEAR_ALL_ITEMS:
       return basketState;
 
